60c81a3b82857c3c68f8e449269e9f3881a7586b
Juan Diego Castillo Carnero <juandiegomaster13@hotmail.com>
2023-11-06 17:03:16 -0500

    spawner básico funcional

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public EnemyController enemyPrefab;
+    public float trajectoryVariance = 15.0f;
+    public float spawnRate = 2.0f;
+    public float spawnDistance = 15.0f;
+    public int spawnAmount = 1;
+    private void Start()
+    {
+        InvokeRepeating(nameof(Spawn), this.spawnRate, this.spawnRate);
+    }
+    private void Spawn()
+    {
+        for (int i = 0; i < this.spawnAmount; i++)
+        {
+            UnityEngine.Vector3 spawnDirection = Random.insideUnitSphere.normalized * spawnDistance;
+            UnityEngine.Vector3 spawnPoint = this.transform.position + spawnDirection;
+
+            float variance = Random.Range(-this.trajectoryVariance, this.trajectoryVariance);
+            UnityEngine.Quaternion rotation = UnityEngine.Quaternion.AngleAxis(variance, UnityEngine.Vector3.forward);
+
+            EnemyController enemy = Instantiate(this.enemyPrefab, spawnPoint, rotation);
+        }
+    }
+}
