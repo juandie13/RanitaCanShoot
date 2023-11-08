f180417370127cdb7e802cca6f519b409fc0fca9
MatiasV-26 <73367484+MatiasV-26@users.noreply.github.com>
2023-11-08 13:34:28 -0500

    avance

---
+++ Assets/Scripts/FirstCanvasLogic.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System;
+public class FirstCanvasLogic : MonoBehaviour
+{
+    public int enemyCount;
+    public TextMeshProUGUI text;
+    public string count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCount = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //enemyCount = int.Parse(text.text);
+        //Debug.Log(text.text);
+    }
+    public void Empezar()
+    {
+        //int.TryParse(text.text, out enemyCount);
+        //enemyCount = Convert.ToInt32(text.text.ToString());
+        //enemyCount = int.Parse(text.text.ToString());
+
+        count = text.text;
+        count = count.Substring(0, count.Length - 1);
+        bool success = int.TryParse(count, out enemyCount);
+        if (success)
+        {
+            Debug.Log(enemyCount); // Output: 123
+        }
+        else
+        {
+            Debug.Log("Invalid input");
+        }
+        Debug.Log(enemyCount);
+        SceneManager.LoadScene("MainScene");
+    }
+}
